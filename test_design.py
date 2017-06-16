3674e679b6e3eada59dbe4e03221d48aea968e8f
Jonathan Shor <JonathanShor@gmail.com>
2017-06-16 16:56:38 -0400

    Adds TA session window testing
    TA interacting with a session window as a student would.

--- test_design.py
+++ test_design.py
@@ -57,11 +57,32 @@ def destroy_context(context):
 
     courses_model.Course(context['course_id']).destroy()
 
+
+def add_attendance_records(course, students, num_attendance_recs=1):
+    for i in range(num_attendance_recs):
+        course.open_session()
+        for student in students:
+            _, seid = student.get_secret_and_seid()
+            student.insert_attendance_record(seid)
+        course.close_session(seid)
+    found_attendance_recs = student.get_num_attendance_records(course.cid)
+    assert found_attendance_recs == num_attendance_recs, (
+        "Found {0} attendance records, expected {}.".format(found_attendance_recs, num_attendance_recs))
+
+
+def remove_from_course(student, course):
+    course.remove_student(student.get_uni())     # Test 3.
+    found_attendance_recs = student.get_num_attendance_records(course.cid)
+    assert found_attendance_recs == 0, (
+        "Found {0} attendance records after {1} removed from course.".format(found_attendance_recs,
+            student.get_uni()))
+
+
 def test_TA_promote_demote():
     context = create_common_context()
-
     course = context['course_id']
     ta = context['ta_id']
+
     course.add_student(ta.get_uni())
     assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 1a."
     course.add_TA(ta.get_uni())     # Test 1.
@@ -78,19 +99,66 @@ def test_TA_promote_demote():
 
     destroy_context(context)
 
-
     context = create_common_context()
-
     course = context['course_id']
     ta = context['ta_id']
+
     assert ta not in course.get_students(), "TA reported as enrolled in course after destroy->create context."
     course.add_TA(ta.get_uni())     # Test 1b.
     assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 1a."
     assert ta in course.get_TAs(), "TA not reported as TA for course, fails test 1b., 4a."
 
-    course.remove_student(ta.get_uni())     # Test 3.
+    remove_from_course(ta, course)     # Test 3.
     assert ta not in course.get_students(), "TA reported as enrolled in course after remove_student."
     assert ta not in course.get_TAs(), "TA reported as TA for course after remove_student, fails test 3, 4b-3."
 
+    destroy_context(context)
+
+
+def test_TA_session_window():
+    num_attendance_recs = 3
+    context = create_common_context()
+
+    course = context['course_id']
+    ta = context['ta_id']
+    course.add_TA(ta.get_uni())     # Test 1.
+
+    assert course.get_active_session() == -1
+    add_attendance_records(course, [ta], num_attendance_recs=num_attendance_recs)
+
+    course.remove_TA(ta.get_uni())  # Test 2.
+    found_attendance_recs = ta.get_num_attendance_records(course.cid)
+    assert found_attendance_recs == num_attendance_recs, (
+    "After TA demotion, found {0} attendance records, expected {}.".format(found_attendance_recs, num_attendance_recs))
+
+    course.remove_TA(ta.get_uni())  # Test 2c.
+    found_attendance_recs = ta.get_num_attendance_records(course.cid)
+    assert found_attendance_recs == num_attendance_recs, (
+    "After redundant demotion, found {0} attendance records, expected {}.".format(found_attendance_recs, num_attendance_recs))
+
+    destroy_context(context)
+
+
+    context = create_common_context()
+
+    course = context['course_id']
+    ta = context['ta_id']
+    student = context['student_id']
+
+    assert ta not in course.get_students(), "TA reported as enrolled in course after destroy->create context."
+    course.add_TA(ta.get_uni())     # Test 1b.
+
+    assert course.get_active_session() == -1
+    add_attendance_records(course, [ta, student], num_attendance_recs=num_attendance_recs)
+
+    remove_from_course(ta, course)
+    remove_from_course(student, course)
+
+    for stu in [ta, student]:
+        course.add_TA(stu.get_uni())
+        found_attendance_recs = stu.get_num_attendance_records(course.cid)
+        assert found_attendance_recs == 0, (
+            "Found {0} attendance records after {1} readded to course, test 3a/b.".format(found_attendance_recs,
+                stu.get_uni()))
 
     destroy_context(context)
