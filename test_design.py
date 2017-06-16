a17101992f59a3fbbe4be222eeb03fd165feb4dd
Jonathan Shor <JonathanShor@gmail.com>
2017-06-16 13:50:50 -0400

    TA promote/demote test case

--- test_design.py
+++ test_design.py
@@ -57,9 +57,40 @@ def destroy_context(context):
 
     courses_model.Course(context['course_id']).destroy()
 
-def first_test():
+def test_TA_promote_demote():
     context = create_common_context()
 
-    assert True
+    course = context['course_id']
+    ta = context['ta_id']
+    course.add_student(ta.get_uni())
+    assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 1a."
+    course.add_TA(ta.get_uni())     # Test 1.
+    assert ta in course.get_TAs(), "TA not reported as TA for course, fails test 1., 4a."
+    assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 1a."
+
+    course.remove_TA(ta.get_uni())  # Test 2.
+    assert ta not in course.get_TAs(), "TA still reported as TA for course, fails test 2., 4b-2."
+    assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 2d."
+
+    course.remove_TA(ta.get_uni())  # Test 2c.
+    assert ta not in course.get_TAs(), "TA still reported as TA for course, fails test 2c., 4b-2."
+    assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 2d."
+
+    destroy_context(context)
+
+
+    context = create_common_context()
+
+    course = context['course_id']
+    ta = context['ta_id']
+    assert ta not in course.get_students(), "TA reported as enrolled in course after destroy->create context."
+    course.add_TA(ta.get_uni())     # Test 1b.
+    assert ta in course.get_students(), "TA not reported as enrolled in course, fails test 1a."
+    assert ta in course.get_TAs(), "TA not reported as TA for course, fails test 1b., 4a."
+
+    course.remove_student(ta.get_uni())     # Test 3.
+    assert ta not in course.get_students(), "TA reported as enrolled in course after remove_student."
+    assert ta not in course.get_TAs(), "TA reported as TA for course after remove_student, fails test 3, 4b-3."
+
 
     destroy_context(context)
